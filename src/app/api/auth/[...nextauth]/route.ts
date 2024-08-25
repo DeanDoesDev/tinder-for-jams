54592d9990137330323397f2f129d5c33ca499f3
AlbertDoesDEV <albertdev906@gmail.com>
2024-08-25 11:27:51 -0400

    added prod secret for nextauth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -11,7 +11,8 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         })
-    ]
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
 })
 
 export { handler as GET, handler as POST }
